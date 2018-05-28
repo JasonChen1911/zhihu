ed4574cfd8d6112c92b9930bb4a72f5ee0ff3513
Jason <923927933@qq.com>
2018-05-28 18:17:25 +0800

    代理ip爬取

--- project/ip_proxys_manage.py
+++ project/ip_proxys_manage.py
@@ -1,7 +1,8 @@
+#coding=utf8
 import requests
 from bs4 import BeautifulSoup
 import MySQLdb as mdb
-
+import time
 
 
 def getweb(url):
@@ -17,18 +18,31 @@ def getweb(url):
     except:
         print("Error:status is ", r.raise_for_status)
 
-def Soup(html):
+
+def Soup(url, n, m):
+    html=getweb(url+str(n))
     r_soup=BeautifulSoup(html, 'lxml')
-    json_text = r_soup.body.contents[1].find_all("tbody")[0].find_all("tr")[0]#.find_all("td")
-    for
-    #obj_json = json.loads(json_text)
-    print(json_text)
+    nav_num= r_soup.body.contents[1].find_all("div",{"id":"listnav"})[0].find_all("a")[-1].text
+    tr_text = r_soup.body.contents[1].find_all("tbody")[0].find_all("tr")#.find_all("td")
+    for item in tr_text:
+        ip=item.find("td", {"data-title":"IP"}).text
+        port=item.find("td", {"data-title":"PORT"}).text
+        type=item.find("td", {"data-title":"类型"}).text
+        print(str(m)+" : "+type+":"+ip+":"+str(port))
+        m+=1
+        if m>50:
+            return 0
+    n+=1
+    time.sleep(2)
+    if n==int(nav_num)+1:
+        return 0
+    Soup(url, n, m)
+
+
 
 
 def main():
-    url='https://www.kuaidaili.com/free/inha/3/'
-    html=getweb(url)
-    Soup(html)
-
+    urls=['https://www.kuaidaili.com/free/inha/', 'http://www.xicidaili.com/nn/']
+    Soup(urls[0],1,1)
 if __name__ == '__main__':
     main()
